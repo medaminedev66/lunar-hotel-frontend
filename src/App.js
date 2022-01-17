a3187fc5b91bbe3f0cc6fe44402383dfd80e33f4
Juliana Ifionu <julieifionu@gmail.com>
2022-01-17 13:16:44 +0100

    Resolve merge conflict

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Home from './pages/Home';
 import store from './redux/configureStore';
+import RoomDetails from './components/roomDetails';
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="roomDetails" element={<RoomDetails />} />
       </Routes>
     </BrowserRouter>
   </Provider>
