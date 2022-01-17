0dae56f4d356acd44bff1c080425aacd1c524e09
Daniel M. Matongo <m_matongo@icloud.com>
2022-01-17 17:15:54 +0200

    add logout page with logic

---
+++ src/pages/LogOut.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { login } from '../redux/auth';
+
+const LogOut = () => {
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    dispatch(login(data));
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <form className="col s12" onSubmit={handleSubmit(onSubmit)} method="post">
+          <input className="input" name="name" placeholder="Name" {...register('name', { required: true })} />
+          <input className="input" name="email" placeholder="Email" {...register('email', { required: true })} />
+          <input className="input" name="password" placeholder="Password" {...register('password', { required: true })} />
+          <input className="input" name="password_confirmation" placeholder="Password Confirmation" {...register('password_confirmation', { required: true })} />
+          <input className="input" type="submit" value="Sign Up" />
+        </form>
+        <Link to="/login">Log In</Link>
+      </div>
+    </div>
+  );
+};
+
+export default LogOut;
