fb42f1ee403a296f5b7dfaa37d9fd5281ab959ca
Juliana Ifionu <julieifionu@gmail.com>
2022-01-17 20:20:53 +0100

    Add Lunar to Offcanvas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,7 +28,7 @@ function Home() {
       </div>
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>Lunar</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           Some text as placeholder. In real life you can have the elements you
