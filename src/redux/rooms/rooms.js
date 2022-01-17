57f31c0a2b2427dcfd1d357e1fb2d0ad21957f25
Amine Smahi <md.amine.smahi@gmail.com>
2022-01-17 11:29:59 +0100

    Add rooms reducer and configueStote file

---
+++ src/redux/rooms/rooms.js
@@ -0,0 +1,15 @@
+const initialState = [];
+const FETCH_DATA = 'rooms/FETCH_DATA';
+const UPDATE_STATE = 'rooms/UPDATE_STATE';
+
+
+export const roomsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_DATA:
+      return action.payload;
+    case UPDATE_STATE:
+      return action.payload;
+    default:
+      return state;
+  }
+};
