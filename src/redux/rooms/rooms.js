63f6d5cd00f67416b881f6b46114e67b39eca621
Amine Smahi <md.amine.smahi@gmail.com>
2022-01-18 13:33:10 +0100

    Create getRooms action and configure the roomsReducer

--- src/redux/rooms/rooms.js
+++ src/redux/rooms/rooms.js
@@ -1,8 +1,20 @@
+import * as api from '../../api/api';
+
 const initialState = [];
 const FETCH_DATA = 'rooms/FETCH_DATA';
 const UPDATE_STATE = 'rooms/UPDATE_STATE';
 
-const roomsReducer = (state = initialState, action) => {
+export const dispatchRooms = (payload) => ({
+  type: FETCH_DATA,
+  payload,
+});
+
+export const getRooms = (payload) => async (dispatch) => {
+  const response = await api.getRooms(payload);
+  dispatch(dispatchRooms(response));
+};
+
+export const roomsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_DATA:
       return action.payload;
@@ -12,5 +24,3 @@ const roomsReducer = (state = initialState, action) => {
       return state;
   }
 };
-
-export default roomsReducer;
