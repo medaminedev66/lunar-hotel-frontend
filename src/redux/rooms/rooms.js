9d9de59f34596c054dc7f0c1ae6b0a134d4e572d
Adedamola Shittu <shittuadedamola@rocketmail.com>
2022-01-18 23:33:26 +0100

    Add Action And Reducer To Create New Room

--- src/redux/rooms/rooms.js
+++ src/redux/rooms/rooms.js
@@ -1,6 +1,19 @@
+import { createRoom } from '../../api/api';
+
 const initialState = [];
 const FETCH_DATA = 'rooms/FETCH_DATA';
 const UPDATE_STATE = 'rooms/UPDATE_STATE';
+const CREATE_ROOM = 'rooms/CREATE_ROOM';
+
+export const createNewRoom = (payload) => ({
+  type: CREATE_ROOM,
+  payload,
+});
+
+export const addRoom = (payload) => async (dispatch) => {
+  const response = await createRoom(payload);
+  dispatch(createNewRoom(response));
+};
 
 const roomsReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -8,6 +21,8 @@ const roomsReducer = (state = initialState, action) => {
       return action.payload;
     case UPDATE_STATE:
       return action.payload;
+    case CREATE_ROOM:
+      return [...state, action.payload];
     default:
       return state;
   }
