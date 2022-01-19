7f20c35a371d89f86597cf3f5484aec01573ca75
Juliana Ifionu <julieifionu@gmail.com>
2022-01-19 15:21:05 +0100

    Create reservations reducer and actions

---
+++ src/redux/reservations/reservations.js
@@ -0,0 +1,68 @@
+import * as api from '../../api/api';
+
+// export const loadReservation = (state) => state.auth.currentUser;
+
+const CREATE_RESERVATION = 'reservation/create_reservation';
+const GET_SINGLE_RESERVATION = 'reservation/get_single_reservation';
+const GET_ALL_RESERVATION = 'reservation/get_all_reservation';
+const DELETE_RESERVATION = 'reservation/delete_reservation';
+
+export const createReservationAction = (payload) => ({
+  type: CREATE_RESERVATION,
+  payload,
+});
+
+export const getSingleReservationAction = (payload) => ({
+  type: GET_SINGLE_RESERVATION,
+  payload,
+});
+
+export const getAllReservationAction = (payload) => ({
+  type: GET_ALL_RESERVATION,
+  payload,
+});
+
+export const deleteAReservationAction = (payload) => ({
+  type: DELETE_RESERVATION,
+  payload,
+});
+
+export const reservationCreate = (payload) => async (dispatch) => {
+  const response = await api.createReservation(payload);
+  dispatch(createReservationAction(response));
+};
+
+export const getSingleReservation = (payload) => async (dispatch) => {
+  const response = await api.getReservation(payload);
+  dispatch(getSingleReservationAction(response));
+};
+
+export const getAllReservation = () => async (dispatch) => {
+  const response = await api.getReservations();
+  dispatch(getAllReservationAction(response));
+};
+
+export const deleteAReservation = (payload) => async (dispatch) => {
+  const response = await api.deleteReservation(payload);
+  dispatch(deleteAReservationAction(response));
+};
+
+const initialState = [];
+
+const reservationsReducer = (state = initialState, action) => {
+  let deletedItem = state.filter(id => id !== payload); 
+  switch (action.type) {
+    case CREATE_RESERVATION:
+      return action.payload;
+    case GET_SINGLE_RESERVATION:
+      return action.payload;
+    case GET_ALL_RESERVATION:
+      return action.payload;
+    case DELETE_RESERVATION:
+      return action.payload
+    default:
+      return state;
+  }
+};
+
+export default reservationsReducer;
