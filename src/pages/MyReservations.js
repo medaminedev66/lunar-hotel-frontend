a69f71135f03d583a954f9d4001de96f579d7996
Amine Smahi <md.amine.smahi@gmail.com>
2022-01-21 11:04:11 +0100

    loop through the reservations

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -3,6 +3,7 @@ import lunar from '../images/lunar.png';
 import NavPanel from '../components/NavPanel';
 
 function MyReservation() {
+  const reservations = [1, 2, 3, 4, 5];
   return (
     <div className="home">
       <div className="nav">
@@ -15,6 +16,12 @@ function MyReservation() {
           You are able to cancel the reservation before 24 hours of the reservation
           date
         </h2>
+        <div className="rooms">
+          {reservations.map((room) => (
+            <NavLink to="/room" exact="true" key={room}>
+            </NavLink>
+          ))}
+        </div>
       </div>
     </div>
   );
