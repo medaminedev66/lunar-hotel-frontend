29bc61fcd8f650813bf80fe892e660b20f77358b
Amine Smahi <md.amine.smahi@gmail.com>
2022-01-21 11:02:58 +0100

    Import navpanel to the MyReservation page

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import lunar from '../images/lunar.png';
+import NavPanel from '../components/NavPanel';
 
 function MyReservation() {
-  return <h1>My MyReservation</h1>;
+  return (
+    <div className="home">
+      <div className="nav">
+        <img src={lunar} className="lunar-logo" alt="" />
+        <NavPanel />
+      </div>
+      <div className="main">
+        <h1>My reservations</h1>
+        <h2>
+          You are able to cancel the reservation before 24 hours of the reservation
+          date
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default MyReservation;
