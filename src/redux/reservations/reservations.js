67ace709d923e48ffa4b23fa9857a4b3fc1322e2
Amine Smahi <md.amine.smahi@gmail.com>
2022-01-24 20:59:34 +0100

    Configure redux/reservations

--- src/redux/reservations/reservations.js
+++ src/redux/reservations/reservations.js
@@ -1,43 +1,61 @@
-const CREATE_RESERVATION = 'reservation/create_reservation';
-const GET_SINGLE_RESERVATION = 'reservation/get_single_reservation';
-const GET_ALL_RESERVATION = 'reservation/get_all_reservation';
-const DELETE_RESERVATION = 'reservation/delete_reservation';
+// const CREATE_RESERVATION = 'reservation/create_reservation';
+// const GET_SINGLE_RESERVATION = 'reservation/get_single_reservation';
+// const GET_ALL_RESERVATION = 'reservation/get_all_reservation';
+// const DELETE_RESERVATION = 'reservation/delete_reservation';
 
-export const createReservationAction = (payload) => ({
-  type: CREATE_RESERVATION,
-  payload,
-});
-
-export const getSingleReservationAction = (payload) => ({
-  type: GET_SINGLE_RESERVATION,
-  payload,
-});
-
-export const getAllReservationAction = (payload) => ({
-  type: GET_ALL_RESERVATION,
-  payload,
-});
-
-export const deleteAReservationAction = (payload) => ({
-  type: DELETE_RESERVATION,
-  payload,
-});
+const END_POINT = 'http://localhost:3000';
+const API_ROUTE = '/api/v1/';
 
 const initialState = [];
+const FETCH_DATA = 'reservations/FETCH_DATA';
+const UPDATE_STATE = 'reservations/UPDATE_STATE';
+const CREATE_ROOM = 'reservations/CREATE_ROOM';
 
-const reservationsReducer = (state = initialState, action) => {
+// export const createReservationAction = (payload) => ({
+//   type: CREATE_RESERVATION,
+//   payload,
+// });
+
+// export const getSingleReservationAction = (payload) => ({
+//   type: GET_SINGLE_RESERVATION,
+//   payload,
+// });
+
+// export const getAllReservationAction = (payload) => ({
+//   type: GET_ALL_RESERVATION,
+//   payload,
+// });
+
+// export const deleteAReservationAction = (payload) => ({
+//   type: DELETE_RESERVATION,
+//   payload,
+// });
+
+export const dispatchReservations = (payload) => ({
+  type: FETCH_DATA,
+  payload,
+});
+
+export const getReservations = () => async (dispatch) => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${END_POINT}${API_ROUTE}reservations`, {
+    headers: {
+      Authorization: `${token}`,
+    },
+  });
+  const data = await response.json();
+  dispatch(dispatchReservations(data));
+};
+
+export const reservationsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case CREATE_RESERVATION:
-      return [...state, action.payload];
-    case GET_SINGLE_RESERVATION:
+    case FETCH_DATA:
       return action.payload;
-    case GET_ALL_RESERVATION:
+    case UPDATE_STATE:
+      return action.payload;
+    case CREATE_ROOM:
       return [...state, action.payload];
-    case DELETE_RESERVATION:
-      return [...state, state.filter((r) => r.id !== action.payload)];
     default:
       return state;
   }
 };
-
-export default reservationsReducer;
