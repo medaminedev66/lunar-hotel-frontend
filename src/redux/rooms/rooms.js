a3f52e333fdbca786ea626cf21b4ff97d1456b31
Juliana Ifionu <julieifionu@gmail.com>
2022-01-24 17:49:06 +0100

    Get rooms from store

--- src/redux/rooms/rooms.js
+++ src/redux/rooms/rooms.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-unused-vars */
-import * as api from '../../api/api';
-
+// import * as api from '../../api/api';
 const END_POINT = 'http://localhost:3000';
 const API_ROUTE = '/api/v1/';
-
 const initialState = [];
 const FETCH_DATA = 'rooms/FETCH_DATA';
 const UPDATE_STATE = 'rooms/UPDATE_STATE';
@@ -13,7 +11,6 @@ export const createNewRoom = (payload) => ({
   type: CREATE_ROOM,
   payload,
 });
-
 export const addRoom = (payload) => async (dispatch) => {
   const token = localStorage.getItem('token');
   const response = await fetch(`${END_POINT}${API_ROUTE}rooms`, {
@@ -27,12 +24,10 @@ export const addRoom = (payload) => async (dispatch) => {
   const data = await response.json();
   dispatch(createNewRoom(data));
 };
-
 export const dispatchRooms = (payload) => ({
   type: FETCH_DATA,
   payload,
 });
-
 export const getRooms = () => async (dispatch) => {
   const token = localStorage.getItem('token');
   const response = await fetch(`${END_POINT}${API_ROUTE}rooms`, {
@@ -43,7 +38,6 @@ export const getRooms = () => async (dispatch) => {
   const data = await response.json();
   dispatch(dispatchRooms(data));
 };
-
 export const roomsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_DATA:
