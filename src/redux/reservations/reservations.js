23a4edc8106bf561324673f9a3532bc6d1406a99
Amine Smahi <md.amine.smahi@gmail.com>
2022-01-24 22:30:30 +0100

    create deleteReservation action

--- src/redux/reservations/reservations.js
+++ src/redux/reservations/reservations.js
@@ -1,7 +1,7 @@
 // const CREATE_RESERVATION = 'reservation/create_reservation';
 // const GET_SINGLE_RESERVATION = 'reservation/get_single_reservation';
 // const GET_ALL_RESERVATION = 'reservation/get_all_reservation';
-// const DELETE_RESERVATION = 'reservation/delete_reservation';
+const DELETE_RESERVATION = 'reservation/delete_reservation';
 
 const END_POINT = 'http://localhost:3000';
 const API_ROUTE = '/api/v1/';
@@ -9,7 +9,7 @@ const API_ROUTE = '/api/v1/';
 const initialState = [];
 const FETCH_DATA = 'reservations/FETCH_DATA';
 const UPDATE_STATE = 'reservations/UPDATE_STATE';
-const CREATE_ROOM = 'reservations/CREATE_ROOM';
+const CREATE_RESERVATION = 'reservations/CREATE_ROOM';
 
 // export const createReservationAction = (payload) => ({
 //   type: CREATE_RESERVATION,
@@ -26,10 +26,10 @@ const CREATE_ROOM = 'reservations/CREATE_ROOM';
 //   payload,
 // });
 
-// export const deleteAReservationAction = (payload) => ({
-//   type: DELETE_RESERVATION,
-//   payload,
-// });
+export const deleteAReservationAction = (payload) => ({
+  type: DELETE_RESERVATION,
+  payload,
+});
 
 export const dispatchReservations = (payload) => ({
   type: FETCH_DATA,
@@ -47,14 +47,29 @@ export const getReservations = () => async (dispatch) => {
   dispatch(dispatchReservations(data));
 };
 
+export const deleteReservation = (id) => async (dispatch) => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${END_POINT}${API_ROUTE}reservations/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `${token}`,
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+  dispatch(deleteAReservationAction(id));
+};
+
 export const reservationsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_DATA:
       return action.payload;
     case UPDATE_STATE:
       return action.payload;
-    case CREATE_ROOM:
+    case CREATE_RESERVATION:
       return [...state, action.payload];
+    case DELETE_RESERVATION:
+      return action.payload;
     default:
       return state;
   }
