814458f42b75247607ab964140a8b6391372c2e8
Shittu Adedamola <shittuadedamola@rocketmail.com>
2022-01-24 19:59:22 +0100

    Write Redux Code For Creating A Reservation

--- src/redux/reservations/reservations.js
+++ src/redux/reservations/reservations.js
@@ -3,6 +3,9 @@ const GET_SINGLE_RESERVATION = 'reservation/get_single_reservation';
 const GET_ALL_RESERVATION = 'reservation/get_all_reservation';
 const DELETE_RESERVATION = 'reservation/delete_reservation';
 
+const END_POINT = 'http://localhost:3000';
+const API_ROUTE = '/api/v1/';
+
 export const createReservationAction = (payload) => ({
   type: CREATE_RESERVATION,
   payload,
@@ -22,6 +25,20 @@ export const deleteAReservationAction = (payload) => ({
   type: DELETE_RESERVATION,
   payload,
 });
+
+export const createReservation = (payload) => async (dispatch) => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${END_POINT}${API_ROUTE}reservations`, {
+    method: 'POST',
+    headers: {
+      Authorization: `${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+  const data = await response.json();
+  dispatch(createReservationAction(data));
+};
 
 const initialState = [];
 
