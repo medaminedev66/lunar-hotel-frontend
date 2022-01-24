6d63b0370b8c5cff0aab5e957f4f17dbd810ecc9
Amine Smahi <md.amine.smahi@gmail.com>
2022-01-24 21:29:54 +0100

    Add use Selector to get reservations

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,26 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Reservation() {
+function Reservation(props) {
+  const { reservation } = props;
   return (
     <div className="reservation">
       <div className="co1">
         <div className="check-in tex">
           <h3>Check-in:</h3>
-          <span>12/12/2012</span>
+          <span>{reservation.check_in}</span>
         </div>
         <div className="check-out tex">
           <h3>Check-out:</h3>
-          <span>12/12/2012</span>
+          <span>{reservation.check_out}</span>
         </div>
         <div className="Nights tex">
           <h3>Nights:</h3>
-          <span>3</span>
+          <span>{reservation.nights}</span>
         </div>
       </div>
       <div className="co2">
         <div className="room-id tex">
           <h3>Room-id:</h3>
-          <span>1268N</span>
+          <span>
+            №
+            {reservation.id}
+          </span>
         </div>
         <button type="button" className="btn btn-outline-danger">
           Cancel
@@ -30,4 +35,7 @@ function Reservation() {
   );
 }
 
+Reservation.propTypes = {
+  reservation: PropTypes.instanceOf(Object).isRequired,
+};
 export default Reservation;
--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import lunar from '../images/lunar.png';
 import Reservation from '../components/Reservation';
 import NavPanel from '../components/NavPanel';
 import './reservations.css';
 
 function MyReservation() {
-  const reservations = [1, 2, 3, 4, 5];
+  const reservations = useSelector((state) => state.reservationsReducer);
   return (
     <div className="home">
       <div className="nav">
@@ -20,7 +21,7 @@ function MyReservation() {
         </h2>
         <div className="reservations">
           {reservations.map((reservation) => (
-            <Reservation key={reservation} />
+            <Reservation reservation={reservation} key={reservation.id} />
           ))}
         </div>
       </div>
