1c26a7f5def6a371a65740cdaed3497ca7acf82a
Juliana Ifionu <julieifionu@gmail.com>
2022-01-25 08:32:12 +0100

    Remove rooms price and location

--- src/components/RoomItem.js
+++ src/components/RoomItem.js
@@ -1,77 +1,43 @@
-/* eslint-disable camelcase */
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
-// import RoomDetails from './RoomDetails';
 import image from '../images/pexels-pixabay-164595.jpg';
 
 const RoomItem = ({ room }) => {
   const {
-    name, city, rate, room_type, amenities, id,
+    name, id,
   } = room;
 
-  // const handleClick = (id) => {
-  //   console.log(id);
-  // };
-  return (
-    <Container className="room-item">
-      <Row>
-        <Col>
-          <Image className="room-image" src={image} alt="hotel room" />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          {name && (
-            <h2>
-              Hotel Name:
-              {' '}
-              {name}
-            </h2>
-          )}
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          {city && (
-            <h3>
-              City Location:
-              {' '}
-              {city}
-            </h3>
-          )}
-          {rate && (
-            <h3>
-              24hrs Rate:
-              {' '}
-              $
-              {rate}
-            </h3>
-          )}
-          {room_type && (
-            <h3>
-              Room Type:
-              {' '}
-              {room_type}
-            </h3>
-          )}
-          {amenities && (
-            <h3>
-              Amenities:
-              {' '}
-              {amenities}
-            </h3>
-          )}
-          {id && (
-            <Link to={{ pathname: `room_details/${id}` }}>Click me</Link>
-          )}
-        </Col>
-      </Row>
-    </Container>
-  );
+  if (room !== null) {
+    return (
+      <Container className="room-item">
+        <Row>
+          <Col>
+            <Image className="room-image" src={image} alt="hotel room" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {name && (
+              <h2 className="hotelName">
+                {name}
+              </h2>
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {id && (
+              <Link to={{ pathname: `room_details/${id}` }} className="btn btn-primary upperCase mb-2">Add Room</Link>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+  return <p>Please add a room</p>;
 };
 
 RoomItem.propTypes = {
