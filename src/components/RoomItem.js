85ea070c6174c432347a6d7ec7593aa198621f0d
Juliana Ifionu <julieifionu@gmail.com>
2022-01-25 08:49:15 +0100

    Remove hard coded image and render image from database

--- src/components/RoomItem.js
+++ src/components/RoomItem.js
@@ -3,41 +3,37 @@ import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
-import image from '../images/pexels-pixabay-164595.jpg';
 
 const RoomItem = ({ room }) => {
   const {
-    name, id,
+    name, id, picture,
   } = room;
 
-  if (room !== null) {
-    return (
-      <Container className="room-item">
-        <Row>
-          <Col>
-            <Image className="room-image" src={image} alt="hotel room" />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {name && (
-              <h2 className="hotelName">
-                {name}
-              </h2>
-            )}
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {id && (
-              <Link to={{ pathname: `room_details/${id}` }} className="btn btn-primary upperCase mb-2">Add Room</Link>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-  return <p>Please add a room</p>;
+  return (
+    <Container className="room-item">
+      <Row>
+        <Col>
+          <Image className="room-image" src={picture} alt="hotel room" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {name && (
+            <h2 className="hotelName">
+              {name}
+            </h2>
+          )}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {id && (
+            <Link to={{ pathname: `room_details/${id}` }} className="btn btn-primary upperCase mb-2">Add Room</Link>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 RoomItem.propTypes = {
